6c65f1f676d7725f05c9a1b7d946d57b277f5412
Tanvir-rahman <rahmantanvir25558@gmail.com>
2018-06-25 16:03:05 +0600

    Updates

--- src/components/RemoveAllOptions.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const RemoveAllOptions = (props) => (
-  <div>
-    {<button className="remove-all-btn" onClick={props.deleteOptionsHandler}>Remove All</button>}
-  </div>
-);
-
-export default RemoveAllOptions;
